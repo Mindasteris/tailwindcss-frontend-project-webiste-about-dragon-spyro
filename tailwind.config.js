29850fbff26bbf8d4cb80023302939396a8cd7f6
Mindaugas Šunokas <46446902+Mindasteris@users.noreply.github.com>
2022-11-17 13:29:38 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    fontFamily: {
+      spyro: ['spyro', 'sans-serif'],
+      spyro_bold: ['spyro-bold', 'sans-serif'],
+      spyro_reg: ['spyro-regular', 'sans-serif'],
+      spyro_sub: ['spyro-sub', 'sans-serif'],
+      Montserrat: ['Montserrat', 'sans-serif'],
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
